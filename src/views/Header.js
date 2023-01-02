2b7943f0ae0df70d4dd26643a56a24a4736e1723
Nguyen Toan <toanntq@devcamp.edu.vn>
2023-01-02 10:28:12 +0700

    clean

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,11 +1,9 @@
 
 import Navbar from "../components/Header/Navbar";
-import { SignInNav } from "../components/Header/SignInNav";
 function Header() {
     return (
       <div className="fixed-top pb-5 mb-5">
         <Navbar/>
-        {/* <SignInNav/> */}
       </div>
     );
 }
