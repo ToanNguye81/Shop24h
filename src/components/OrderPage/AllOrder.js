5bdc1dd565c77e41fd2778f0f2eacf0c75168007
Your Name <toanntq@devcamp.edu.vn>
2023-01-14 16:44:00 +0700

    update UI

--- src/components/OrderPage/AllOrder.js
+++ src/components/OrderPage/AllOrder.js
@@ -7,17 +7,37 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Products from "../../database_sample/productsData.json"
-import { Button,  ButtonGroup, Grid, IconButton, TableFooter, TextField } from '@mui/material';
+import { Button, ButtonGroup, Grid, IconButton, TableFooter, TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { ProductList } from '../../pages/ProductList';
 import { useNavigate } from 'react-router-dom';
 import { fontWeight } from '@mui/system';
+import styled from 'styled-components';
 
+const MyButton = styled.button`
+height:45px;
+width:45px;
+border:0.1px solid #E6E6E6;
+color:#555555;
+background-color:white;
+`
+
+const MyTextBox = styled.input`
+height:45px;
+width:45px;
+color:#555555 ;
+border:none;
+border-top:0.1px solid #E6E6E6;
+border-bottom:0.1px solid #E6E6E6;
+background-color:#F0F0F0
+`
 
 export const AllOrder = () => {
+
+
     const navigate = useNavigate()
-    const onBtnBuyClick=()=>{
+    const onBtnBuyClick = () => {
         navigate("/products")
     }
 
@@ -28,15 +48,15 @@ export const AllOrder = () => {
                 justifyContent="center"
                 alignItems="center"
             >
-                <Grid item xs={12} md={9} >
+                <Grid item xs={11} md={12} >
                     <TableContainer component={Paper}>
                         <Table>
                             <TableHead>
-                                <TableRow >
-                                    <TableCell ><strong>Products</strong></TableCell>
-                                    <TableCell align="right"><strong>Price</strong></TableCell>
-                                    <TableCell align="center"><strong>Quantity</strong></TableCell>
-                                    <TableCell align="right"><strong>Total &nbsp;($)</strong></TableCell>
+                                <TableRow>
+                                    <TableCell align="center"><h5><strong>Products</strong></h5></TableCell>
+                                    <TableCell align="center"><h5><strong>Price</strong></h5></TableCell>
+                                    <TableCell align="center"><h5><strong>Quantity</strong></h5></TableCell>
+                                    <TableCell align="center"><h5><strong>Total &nbsp;($)</strong></h5></TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
@@ -46,57 +66,46 @@ export const AllOrder = () => {
                                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
                                         <TableCell component="th" scope="item" width="25%">
-                                            <img src={item.imageUrl} width="100" />
-                                            <strong>{item.name}</strong>
-                                        </TableCell>
-                                        <TableCell align="right" width="25%">{item.buyPrice}</TableCell>
-                                        <TableCell align="center" width="25%">
-
-                                            {/* <Grid container spacing={{ xs: 2, md: 3 }} sx={{border:"1px solid #E6E6E6"}} columns={{ xs: 4, sm: 8, md: 12 }}>
-                                                <Grid item sx={{borderRight:"1px solid #E6E6E6"}} textAlign="center">
-                                                <AddIcon/>
+                                            <Grid container
+                                                direction="column"
+                                                justifyContent="flex-start"
+                                                alignItems="center">
+                                                <img src={item.imageUrl} width="100" />
+                                                <Grid item textAlign="center">
+                                                <strong>{item.name}</strong>
                                                 </Grid>
-                                                <Grid item >sadfh</Grid>
-                                                <Grid item sx={{borderLeft:"1px solid #E6E6E6"}}>_</Grid>
-                                            </Grid> */}
-
-                                            <ButtonGroup>
-                                                <Button sx={{height:"50px",width:"50px",border:"0.1px solid #E6E6E6",color:"#555555"}}><AddIcon/></Button>
-                                                <input style={{height:"50px",width:"50px",backgroundColor:"#F7F7F7",color:"#555555" ,border:"0.1px solid #E6E6E6"}}></input>
-                                                <Button sx={{height:"50px",width:"50px",border:"0.1px solid #E6E6E6",color:"#555555"}}><RemoveIcon/></Button>
-                                            </ButtonGroup>
-
-                                            {/* <IconButton>
-                                                <AddIcon/>
-                                            </IconButton>
-                                            <TextField sx={{ width: "40%", textAlign: "center" }} value="50" />
-                                            <IconButton >
-                                               
-                                            </IconButton> */}
+                                            </Grid>
                                         </TableCell>
-                                        <TableCell align="right" width="25%"><del>{item.promotionPrice}</del></TableCell>
+                                        <TableCell align="center" width="25%">{item.buyPrice}</TableCell>
+                                        <TableCell align="center" width="25%">
+                                            <ButtonGroup>
+                                                <MyButton><AddIcon /></MyButton>
+                                                <MyTextBox></MyTextBox>
+                                                <MyButton><RemoveIcon /></MyButton>
+                                            </ButtonGroup>
+                                        </TableCell>
+                                        <TableCell align="center" width="25%">{item.promotionPrice}</TableCell>
                                     </TableRow>
                                 ))}
                                 <TableRow>
-                                    <TableCell align="left" colSpan={1}>
-                                        Total
-                                    </TableCell>
-                                    <TableCell align="right" colSpan={4}>
-                                    <strong>12322$</strong>
-                                    </TableCell>
+                                    <Grid container direction="column">
+                                   <TextField placeholder="Coupon Code"></TextField>
+                                   <Button>Apply coupon</Button>
+                                   <Button>Update Cart</Button>
+                                    </Grid>
                                 </TableRow>
                             </TableBody>
                         </Table>
                     </TableContainer>
                 </Grid>
             </Grid>
-            <Grid container item sx={{mt:5, mb:5 ,display:{xs:12, md:8}}}
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="stretch">
-                    <Button onClick={onBtnBuyClick} class="btn col-sm-2 btn-warning rounded-pill pb-3 pt-3">Mua tiếp</Button>
-                    <Button class="btn col-sm-2 btn-danger rounded-pill pb-3 pt-3">Đặt hàng</Button>
-                </Grid>
+            <Grid container item sx={{ mt: 5, mb: 5, display: { xs: 12, md: 8 } }}
+                direction="row"
+                justifyContent="center"
+                alignItems="stretch">
+                <Button onClick={onBtnBuyClick} class="btn col-sm-2 btn-warning rounded-pill pb-3 pt-3">Mua tiếp</Button>
+                <Button class="btn col-sm-2 btn-danger rounded-pill pb-3 pt-3">Đặt hàng</Button>
+            </Grid>
         </React.Fragment>
 
     );
--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,22 +1,23 @@
 import { Grid } from "@mui/material"
 import React from "react"
+import { Container } from "react-bootstrap"
 import { AllOrder } from "../components/OrderPage/AllOrder"
 import { CartTotal } from "../components/OrderPage/CartTotal"
 
 export const OrderPage = () => {
     return (
         <React.Fragment>
-            <Grid container
-                direction="row"
-                justifyContent="space-between"
-                alignItems="flex-start" mt={20}>
-                <Grid item xs={12} md={8}>
-                    <AllOrder />
+                <Grid container
+                    direction="row"
+                    justifyContent="space-evenly"
+                    alignItems="flex-start" mt={20}>
+                    <Grid item xs={12} md={6.5}>
+                        <AllOrder />
+                    </Grid>
+                    <Grid item xs={12} md={3.5}>
+                        <CartTotal />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    <CartTotal />
-                </Grid>
-            </Grid>
         </React.Fragment>
     )
 }
