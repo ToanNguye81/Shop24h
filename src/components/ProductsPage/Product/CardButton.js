2c50f93b5b162ebddba3303063497f78ec23b792
Your Name <toanntq@devcamp.edu.vn>
2023-03-09 05:57:50 +0700

    update card

--- src/components/ProductsPage/Product/CardButton.js
+++ src/components/ProductsPage/Product/CardButton.js
@@ -3,72 +3,76 @@ import { styled } from '@mui/material/styles';
 import ButtonBase from '@mui/material/ButtonBase';
 import Typography from '@mui/material/Typography';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { IconButton } from '@mui/material';
 
 const StyledProductImg = styled('img')({
-  top: 0,
-  width: '100%',
-  height: '100%',
-  objectFit: 'cover',
-  position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    position: 'absolute',
 });
 
 
 // ----------------------------------------------------------------------
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  top: 0,
-  width: '100%',
-  height: '100%',
-  objectFit: 'cover',
-  position: 'absolute',
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important', // Overrides inline-style
-  },
-  '&:hover, &.Mui-focusVisible': {
-    zIndex: 1,
-    '& .MuiImageBackdrop-root': {
-      opacity: "30%",
+    top: 0,
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    position: 'absolute',
+    [theme.breakpoints.down('sm')]: {
+        width: '100% !important', // Overrides inline-style
     },
-    '& .MuiImageMarked-root': {
-      opacity: "0",
+    '&:hover, &.Mui-focusVisible': {
+        zIndex: 1,
+        '& .MuiImageBackdrop-root': {
+            opacity: "30%",
+        },
+        '& .MuiImageMarked-root': {
+            opacity: "0",
+        },
+        '& .MuiTypography-root': {
+            border: '5px solid currentColor',
+        },
+        '& .IconCart': {
+            opacity: "100%",
+        },
     },
-    '& .MuiTypography-root': {
-      border: '5px solid currentColor',
-    },
-  },
 }));
 
 const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common.white,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
 }));
 
 const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundColor: theme.palette.common.black,
-  opacity: "0%",
-  transition: theme.transitions.create('opacity'),
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: "0%",
+    transition: theme.transitions.create('opacity'),
 }));
 
 
-export default function CardButton({imageUrl}) {
+export default function CardButton({ imageUrl }) {
     return (
         <ImageButton
             focusRipple
         >
             <StyledProductImg src={imageUrl} />
-            <ImageBackdrop className="MuiImageBackdrop-root"/>
+            <ImageBackdrop className="MuiImageBackdrop-root" />
             <Image>
                 <Typography
                     component="span"
@@ -81,7 +85,9 @@ export default function CardButton({imageUrl}) {
                         pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
                     }}
                 >
-                    <AddShoppingCartIcon/>
+                    <IconButton>
+                        <AddShoppingCartIcon className='IconCart' sx={{color:"white", opacity:"0"}}/>
+                    </IconButton>
                 </Typography>
             </Image>
         </ImageButton>
