4490f62b7c968f5111bd6ad069f2ca00f962beff
Your Name <toanntq@devcamp.edu.vn>
2023-03-27 17:51:33 +0700

    update cart V2

--- src/reducers/cart.reducers.js
+++ src/reducers/cart.reducers.js
@@ -16,6 +16,112 @@ const initialState = {
     cartCost: 0,
 }
 
+export const cartReducers = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART: {
+            const { product } = action.payload;
+            const itemIndexToAdd = state.cart.findIndex((item) => item.product && item.product._id === product._id);
+
+            let newCart;
+            if (itemIndexToAdd === -1) {
+                // Sản phẩm chưa có trong giỏ hàng
+                newCart = [...state.cart, { product: product, quantity: 1 }];
+            } else {
+                // Sản phẩm đã có trong giỏ hàng
+                newCart = [...state.cart];
+                newCart[itemIndexToAdd].quantity++;
+            }
+
+            localStorage.setItem("cart", JSON.stringify(newCart));
+            return { ...state, cart: newCart };
+        }
+
+        // Giảm số lượng sản phẩm trong giỏ hàng
+        case DECREASE_QUANTITY: {
+            const productIdToDecrease = action.payload;
+            const itemIndexToDecrease = state.cart.findIndex((item) => item.product._id === productIdToDecrease);
+            if (itemIndexToDecrease !== -1) {
+                const newCart = [...state.cart];
+                const newQuantity = newCart[itemIndexToDecrease].quantity - 1;
+
+                if (newQuantity === 0) {
+                    newCart.splice(itemIndexToDecrease, 1);
+                } else {
+                    newCart[itemIndexToDecrease].quantity = newQuantity;
+                }
+
+                localStorage.setItem("cart", JSON.stringify(newCart));
+                return { ...state, cart: newCart };
+            } else {
+                return state;
+            }
+        }
+
+        // Xóa sản phẩm khỏi giỏ hàng
+        case REMOVE_FROM_CART:
+            const productIdToRemove = action.payload;
+            const itemIndexToRemove = state.cart.findIndex((item) => item.product._id === productIdToRemove);
+
+            if (itemIndexToRemove !== -1) {
+                // sản phẩm có trong giỏ hàng
+                const newCart = [...state.cart];
+                newCart.splice(itemIndexToRemove, 1);
+                localStorage.setItem("cart", JSON.stringify(newCart));
+                return { ...state, cart: newCart };
+            } else {
+                return state;
+            }
+
+
+        // Thay số lượng sản phẩm trong giỏ hàng
+        case UPDATE_QUANTITY:
+            const { productIdToUpdate, newQuantity } = action.payload;
+            const itemIndexToUpdate = state.cart.findIndex(
+                (item) => item.product._id === productIdToUpdate
+            );
+            let newCart;
+            if (itemIndexToUpdate !== -1) {
+                // Nếu sản phẩm tồn tại
+                newCart = [...state.cart];
+                newCart[itemIndexToUpdate].quantity = newQuantity;
+            } else {
+                return state;
+            }
+
+            if (Number.isInteger(newQuantity) && newQuantity >= 1) {
+                localStorage.setItem("cart", JSON.stringify(newCart));
+            }
+
+            return { ...state, cart: newCart };
+
+        // Tăng số lượng sản phẩm trong giỏ hàng
+        case INCREASE_QUANTITY:
+            const productIdToIncrease = action.payload;
+            const itemIndexToIncrease = state.cart.findIndex((item) => item.product._id === productIdToIncrease);
+
+            if (itemIndexToIncrease !== -1) {
+                // The product is not in the cart yet
+                const newCart = [...state.cart];
+                newCart[itemIndexToIncrease].quantity++;
+                localStorage.setItem("cart", JSON.stringify(newCart));
+                return { ...state, cart: newCart };
+            } else {
+                return state;
+            }
+
+
+        case CHANGE_CART_COST:
+            return {
+                ...state,
+                cartCost: action.cartCost
+            };
+
+        default:
+            return state;
+    }
+};
+
+
 // export const cartReducers = (state = initialState, action) => {
 //     switch (action.type) {
 //         case ADD_FIRST_PRODUCT:
@@ -64,102 +170,5 @@ const initialState = {
 //             return state;
 //     }
 // }
-
-export const cartReducers = (state = initialState, action) => {
-    switch (action.type) {
-        // Thêm sản phẩm vào giỏ hàng
-        case ADD_TO_CART:
-            const { product } = action.payload;
-            const itemIndexToAdd = state.cart.findIndex((item) => item.product && item.product._id === product._id);
-
-            if (itemIndexToAdd === -1) {
-                // Sản phẩm chưa có trong giỏ hàng
-                const newCart = [...state.cart, { product: product, quantity: 1 }];
-                localStorage.setItem("cart", JSON.stringify(newCart));
-                return { ...state, cart: newCart };
-            } else {
-                // Sản phẩm đã có trong giỏ hàng
-                const newCart = [...state.cart];
-                newCart[itemIndexToAdd].quantity++;
-                localStorage.setItem("cart", JSON.stringify(newCart));
-                return { ...state, cart: newCart };
-            }
-
-        // Giảm số lượng sản phẩm trong giỏ hàng
-        case DECREASE_QUANTITY:
-            const productIdToDecrease = action.payload;
-            const itemIndexToDecrease = state.cart.findIndex((item) => item.product._id === productIdToDecrease);
-            if (itemIndexToDecrease !== -1) {
-                const newCart = [...state.cart];
-                const newQuantity = newCart[itemIndexToDecrease].quantity - 1;
-
-                if (newQuantity === 0) {
-                    newCart.splice(itemIndexToDecrease, 1);
-                } else {
-                    newCart[itemIndexToDecrease].quantity = newQuantity;
-                }
-
-                localStorage.setItem("cart", JSON.stringify(newCart));
-                return { ...state, cart: newCart };
-            } else {
-                return state;
-            }
-
-        // Xóa sản phẩm khỏi giỏ hàng
-        case REMOVE_FROM_CART:
-            const productIdToRemove = action.payload;
-            const itemIndexToRemove = state.cart.findIndex((item) => item.product._id === productIdToRemove);
-
-            if (itemIndexToRemove !== -1) {
-                // sản phẩm có trong giỏ hàng
-                const newCart = [...state.cart];
-                newCart.splice(itemIndexToRemove, 1);
-                localStorage.setItem("cart", JSON.stringify(newCart));
-                return { ...state, cart: newCart };
-            } else {
-                return state;
-            }
-
-
-        // Thay số lượng sản phẩm trong giỏ hàng
-        case UPDATE_QUANTITY:
-            const { productIdToUpdate, newQuantity } = action.payload;
-            const itemIndexToUpdate = state.cart.findIndex((item) => item.product._id === productIdToUpdate);
-
-            if (itemIndexToUpdate !== -1) {
-                // Nếu sản phẩm tồn tại
-                const newCart = [...state.cart];
-                newCart[itemIndexToUpdate].quantity = newQuantity;
-                localStorage.setItem("cart", JSON.stringify(newCart));
-                return { ...state, cart: newCart };
-            } else {
-                return state;
-            }
-
-        // Tăng số lượng sản phẩm trong giỏ hàng
-        case INCREASE_QUANTITY:
-            const productIdToIncrease = action.payload;
-            const itemIndexToIncrease = state.cart.findIndex((item) => item.product._id === productIdToIncrease);
-
-            if (itemIndexToIncrease !== -1) {
-                // The product is not in the cart yet
-                const newCart = [...state.cart];
-                newCart[itemIndexToIncrease].quantity++;
-                localStorage.setItem("cart", JSON.stringify(newCart));
-                return { ...state, cart: newCart };
-            } else {
-                return state;
-            }
-        case CHANGE_CART_COST:
-            return {
-                ...state,
-                cartCost: action.cartCost
-            };
-
-        default:
-            return state;
-    }
-};
-
 
 
